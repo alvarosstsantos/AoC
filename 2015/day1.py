8ccf6c730a06b7122e3a68f8920173481bb44408
Alvaro Santos <alvarosstsantos@gmail.com>
2024-07-06 09:29:16 -0300

    :recycle: reduce code size using reduce

--- 2015/day1.py
+++ 2015/day1.py
@@ -1,18 +1,25 @@
 import sys
+from functools import reduce
 
 instructions: str = sys.argv[1]
 
-floor = 0
 
-for idx, i in enumerate(instructions, start = 1):
-    if i == "(":
-        floor += 1
-    elif i == ")":
-        floor -= 1
-    else:
-        print("Invalid Instruction", i)
-        break
+def challenge_1():
+    print(reduce(lambda x, y: x + 1 if y == "(" else x - 1, instructions, 0))
 
-    if floor <= -1:
-        print(idx)
-        break
+
+def challenge_2():
+    floor = 0
+
+    for idx, i in enumerate(instructions, start=1):
+        if i == "(":
+            floor += 1
+        else:
+            floor -= 1
+
+        if floor <= -1:
+            print(idx)
+            break
+
+
+challenge_1()
