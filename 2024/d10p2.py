ab9637d7c3e786f9ae52d8ba1586004f88906283
Alvaro Santos <alvarosstsantos@gmail.com>
2024-12-12 08:02:38 -0300

    :sparkles: advent of code 2024 day 10 problem 2 and :bug: fix dumb recursion error

--- 2024/d10p2.py
+++ 2024/d10p2.py
@@ -1,27 +1,26 @@
 import sys
-from typing import Set, Tuple
 
 
 def main(text: str) -> int:
     A = [[int(char) for char in line] for line in text.splitlines()]
     m, n = len(A), len(A[0])
 
-    def follow_trails(x: int, y: int) -> Set[Tuple[int, int]]:
+    def count_trails(x: int, y: int) -> int:
         nonlocal A, m, n
 
-        z = set()
+        z = 0
 
         if A[x][y] == 9:
-            z.update({(x, y)})
+            z += 1
         else:
             if x > 0 and A[x-1][y] == A[x][y] + 1:
-                z.update(follow_trails(x-1, y))
+                z += count_trails(x-1, y)
             if y > 0 and A[x][y-1] == A[x][y] + 1:
-                z.update(follow_trails(x, y-1))
+                z += count_trails(x, y-1)
             if x < m - 1 and A[x+1][y] == A[x][y] + 1:
-                z.update(follow_trails(x+1, y))
+                z += count_trails(x+1, y)
             if y < n - 1 and A[x][y+1] == A[x][y] + 1:
-                z.update(follow_trails(x, y+1))
+                z += count_trails(x, y+1)
 
         return z
 
@@ -30,14 +29,14 @@ def main(text: str) -> int:
     for i in range(m):
         for j in range(n):
             if A[i][j] == 0:
-                b += len(follow_trails(i, j))
+                b += count_trails(i, j)
 
     return b
 
 
 def test():
     input = "89010123\n78121874\n87430965\n96549874\n45678903\n32019012\n01329801\n10456732"
-    assert 36 == main(input)
+    assert 81 == main(input)
 
 
 if __name__ == "__main__":
